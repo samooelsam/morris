4427f84f1eebbb8d8bef41178cb1fe596cc18f33
saman <saman.tohidian@techvertu.co.uk>
2024-02-27 13:22:04 +0000

    wp config file has changed

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'morris' );
+define( 'DB_NAME', 'morisnewdb' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'morisnewdbuser' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '1Hns6?l3lylY1fqEx' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
