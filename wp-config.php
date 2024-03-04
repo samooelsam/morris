69d4c52091facee17dc834c5dc6e4c203e14898a
saman <saman.tohidian@techvertu.co.uk>
2024-03-04 15:06:59 +0000

    add archive and category page

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'admin_tech' );
+define( 'DB_NAME', 'morris' );
 
 /** Database username */
-define( 'DB_USER', 'admin_tech' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'Admin@123!@#' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
