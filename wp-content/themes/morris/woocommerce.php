619b775281ac0d52be01d5b1377d5fbce4417915
saman <saman.tohidian@techvertu.co.uk>
2024-03-07 12:07:23 +0000

    fix woocommerce banner bug

--- wp-content/themes/morris/woocommerce.php
+++ wp-content/themes/morris/woocommerce.php
@@ -3,37 +3,40 @@
 	<?php if(!is_single()) {
 		global $wp_query;
 		$tax = get_queried_object();
-		if($tax->count != 0) {?>
-	<figure class="image-wrapper woocommerce-banner clearfix">
-		<?php get_template_part('includes/productsCategoryBanner', 'widget');?>
-		<figcaption class="image-wrapper clearfix">
-			<div class="centerize grid_12 clearfix">
-				<h3><?php 
-				if( is_product_category() ) {
-					$thisPage = $wp_query->get_queried_object()->name;
-					echo($thisPage);
-				}
-				else if($tax){
-					echo($tax->name);
-				}
-				else{
-					$thisPage = get_post(wc_get_page_id( 'shop' ));
-					echo($thisPage->post_name);
-				}?></h3>
-				<?php 
-				get_template_part('includes/taxDescription', 'widget');
-				if (class_exists('WooCommerce') && is_woocommerce()) :
-    
-					woocommerce_breadcrumb(); 
-			 
-		   		endif;
-				?>
+		
+			?>
+		<figure class="image-wrapper woocommerce-banner clearfix">
+			<?php get_template_part('includes/productsCategoryBanner', 'widget');?>
+			<figcaption class="image-wrapper clearfix">
+				<div class="centerize grid_12 clearfix">
+					<h3><?php 
+					if( is_product_category() ) {
+						$thisPage = $wp_query->get_queried_object()->name;
+						
+						echo($thisPage);
+					}
+					else if($tax){
+						echo($tax->name);
+					}
+					else{
+						$thisPage = get_post(wc_get_page_id( 'shop' ));
+						
+						echo($thisPage->post_name);
+					}?></h3>
+					<?php 
+					get_template_part('includes/taxDescription', 'widget');
+					if (class_exists('WooCommerce') && is_woocommerce()) :
+		
+						woocommerce_breadcrumb(); 
 				
-			</div>
-			
-		</figcaption>
-	</figure>
-	<?php }
+					endif;
+					?>
+					
+				</div>
+				
+			</figcaption>
+		</figure>
+	<?php 
 	}
 	if($tax->taxonomy == 'pa_brands'){
 		?>
