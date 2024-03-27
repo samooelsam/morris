b7162f20c3851f1224033cd8cd5bf0216a7b1016
saman <saman.tohidian@techvertu.co.uk>
2024-03-27 14:44:45 +0000

    add banner to category page

--- wp-content/themes/morris/category.php
+++ wp-content/themes/morris/category.php
@@ -11,6 +11,37 @@ if($currentCat->term_id){
             'cat' => $currentCat->term_id,
             'order' => 'DESC', 
         );?>
+        <figure class="image-wrapper woocommerce-banner clearfix">
+			<?php get_template_part('includes/productsCategoryBanner', 'widget');?>
+			<figcaption class="image-wrapper clearfix">
+				<div class="centerize grid_12 clearfix">
+					<h1><?php 
+					if( is_product_category() || is_category()) {
+						$thisPage = $wp_query->get_queried_object()->name;
+						
+						echo($thisPage);
+					}
+					else if($tax){
+						echo($tax->name);
+					}
+					else{
+						$thisPage = get_post(wc_get_page_id( 'shop' ));
+						
+						echo($thisPage->post_name);
+					}?></h1>
+					<?php 
+					get_template_part('includes/taxDescription', 'widget');
+					if (class_exists('WooCommerce') && is_woocommerce()) :
+		
+						woocommerce_breadcrumb(); 
+				
+					endif;
+					?>
+					
+				</div>
+				
+			</figcaption>
+		</figure>
 			<div class="techvertu-news-wrapper clearfix archive-news">
 				<div class="centerize clearfix">
 					<div class="techvertu-news-container clearfix swiper-container">
